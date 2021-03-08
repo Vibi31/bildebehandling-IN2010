04761038f81c1971db6a29fc0842c21bac57a5e2
vibishar <vibishar@mail.uio.no>
2021-03-08 16:04:07 +0100

    question 1 done

--- Uke2/testIN2070.py
+++ Uke2/testIN2070.py
@@ -3,10 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi, cos, sin, mean, sqrt
 
-
 img = imread('portrett.png', as_gray=True)
-m1 = 127  #ønsket middelverdi
-b = m1 - mean(img) #økning for gråtone til hver piksel
 N, M = img.shape
 pix = N*M
 
@@ -26,36 +23,34 @@ def deviation(img): #calculates deviation without using libraries
     return sqrt(tot/pix)
 
 def lin_mean(img, m1): #tar in bilde og ønsket middel verdi
-    f_out = np.zeros((N,M))
+    f_mean = np.zeros((N,M))
     m0 = middel(img)
     for i in range(N):
         for j in range(M):
-            f_out[i, j] = img[i, j] + (m1 - m0)
-    return f_out
+            f_mean[i, j] = img[i, j] + (m1 - m0)
+    return f_mean
 
 def lin_std(img, std1):
     std0 = deviation(img)
     C = std1/std0
-    f_out = np.zeros((N,M))
+    f_std = np.zeros((N,M))
     for i in range(N):
         for j in range(M):
-            f_out[i, j] = img[i, j]*C
-    return f_out
+            f_std[i, j] = img[i, j]*C
+    return f_std
 
 def lin_t(img, std1, mv1): #tar in bilde, ønsket sigma og middelverdi
     std0, mv0 = deviation(img), middel(img)
     C = std1/std0
-    N,M = img.shape
-    f_out = np.zeros((N,M))
+    f_lin = np.zeros((N,M))
     for i in range(N):
         for j in range(M):
-            f_out[i, j] = mv1 + (img[i,j] - mv0) * (std1/std0)
-    return f_out
-
+            f_lin[i, j] = (img[i,j] - mv0) * (std1/std0) +  mv1
+    return f_lin
 
 img_n = lin_t(img, 64, 127)
-
-
+img_b = lin_mean(img, 1800)
+print(img)
 print('middelverdi (original)', middel(img))
 #print(np.mean(img)) #sjekker med ferdig pakke
 print('sigma (original):', deviation(img))
@@ -64,14 +59,11 @@ print('sigma (original):', deviation(img))
 print('sigma (new image):', deviation(img_n))
 print('middelverdi (new image):', middel(img_n))
 
-
-
 plt.figure()
 plt.title("Original")
 plt.imshow(img, cmap='gray')
 
 plt.figure()
-plt.title("linearly transformed (grey tone)")
-plt.imshow(img_n, cmap='gray')
-
+plt.title("gråtone transform")
+plt.imshow(img_n, cmap= 'gray', vmin=0, vmax=255)
 plt.show()
